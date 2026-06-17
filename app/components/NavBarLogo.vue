db0debd7760af289df5df0b873cde3e96fd4366e
zn-har <zenharmuhammed00@gmail.com>
2026-06-17 14:45:25 +0530

    feat: add NavBarLogo component with supporting SVG assets

--- app/components/NavBarLogo.vue
+++ app/components/NavBarLogo.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="flex items-center shrink-0">
     <NuxtImg
-      src="/Images/Logo_letters.png"
+      src="/Images/Logo_letters.svg"
       alt="Macawoo"
       preload
-      format="webp"
       class="w-[150px] h-[46px] object-contain md:block hidden"
     />
     <NuxtImg
-      src="/Images/Logo_letters.png"
+      src="/Images/Logo_letters.svg"
       alt="Macawoo"
       preload
-      format="webp"
       class="h-9 w-auto md:hidden block"
     />
   </div>
